07ef1759048bc41e165ff6b4c2f15399f74cf9ae
singlarobin <singlarobin99@gmail.com>
2025-03-06 14:35:38 +0530

    feat: added comments in test cases

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -10,24 +10,29 @@ describe("Home Page of App", () => {
     });
 
     it("contains story list", () => {
+        //Check if story list container is visible
         cy.get(`#story-list-container`).should("exist");
 
+        //Check if story list container contains the user having stories
         cy.get(`#story-list-container`)
             .find(`[data-cy="story-card-container"]`)
             .should("exist");
 
+        //Check if the Curret User Card contains create button to add new story
         cy.get(`#story-list-container`)
             .find(`[data-cy="story-card-container"]`)
             .first()
             .find("#create-story-btn")
             .should("exist");
 
+        //Check if the last User Card does not contains create button to add new story
         cy.get(`#story-list-container`)
             .find(`[data-cy="story-card-container"]`)
             .last()
             .find("#create-story-btn")
             .should("not.exist");
 
+        //Check if on clicking the current user story card changes the url and opens its posted stories.
         cy.get("#story-list-container")
             .find(`[data-cy="story-card-container"]`)
             .eq(0)
--- cypress/e2e/storiesPage.cy.ts
+++ cypress/e2e/storiesPage.cy.ts
@@ -10,59 +10,60 @@ describe("Stories Page of App", () => {
     });
 
     it("contains User stories", () => {
+        //Check if the story container is visible
         cy.get(`#story-container`).should("exist");
 
+        //Check if the progress bar is visible
         cy.get(`#story-container`)
             .find(`.progress-bar-container`)
             .should("exist");
 
+        // Check if the Profile container is visible
         cy.get(`#story-container`).find(`.profile-container`).should("exist");
 
+        //Check if the transparent element is visible for navigation
         cy.get(`#story-container`).find(`.story-navigation`).should("exist");
 
-        cy.get(`#story-container`)
-            .find(`.story-navigation`)
-            .find(".left-click")
-            .should("exist");
-
-        cy.get(`#story-container`)
-            .find(`.story-navigation`)
-            .find(".right-click")
-            .should("exist");
-
+        //Check if the 1st story contains active class
         cy.get(`#story-container`)
             .find(`.progress-bar-container`)
             .first()
             .find(".active")
             .should("exist");
 
+        // Simulate the click on the right side of the navigation container (transparent element)
         cy.get(`#story-container`)
             .find(`.story-navigation`)
             .find(".right-click")
             .click();
 
+        // Check if the 2nd story contains the active class
         cy.get(`#story-container`)
             .find(`.progress-bar-container`)
             .last()
             .find(".active")
             .should("exist");
 
+        // Simulate the click on the left side of the navigation container (transparent element)
         cy.get(`#story-container`)
             .find(`.story-navigation`)
             .find(".left-click")
             .click();
 
+        // Check if the 1st story contains the active class
         cy.get(`#story-container`)
             .find(`.progress-bar-container`)
             .first()
             .find(".active")
             .should("exist");
 
+        // Simulate the click on the left side of the navigation container (transparent element)
         cy.get(`#story-container`)
             .find(`.story-navigation`)
             .find(".left-click")
             .click();
 
+        //Check if the url is changed to home page (default one)
         cy.url().should("eq", "http://localhost:5173/");
     });
 });
